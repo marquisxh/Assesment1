28b02a67ca4a1a151071d9a8b5dbdcff71a1e281
marquisxh <100623172+marquisxh@users.noreply.github.com>
2022-03-04 12:06:51 -0600

    Delete password.js

--- password.js
+++
@@ -1,9 +0,0 @@
-const readline = require("readline");
-
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-reader.question("Welcome to password validator tool", function(input){
-	tokens = input.split(' '); 
-
